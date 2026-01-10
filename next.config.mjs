930b5f5102690f2f6370ac30dd08ee0f77634602
aiyynakeres <232609604+aiyynakeres@users.noreply.github.com>
2026-01-10 13:48:42 +0900

    Add root to next js config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 import createMDX from '@next/mdx'
- 
+import path from 'path'
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  turbopack: {
+    root: path.join(__dirname, '.')
+  },
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below
